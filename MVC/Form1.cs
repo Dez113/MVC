d8595c0140728065775ec2bde891e00ddd10852b
Dez113 <dlyakontakt@gmail.com>
2017-02-20 17:41:16 +0300

    1

---
+++ MVC/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)                                                    //
+        {                                                                                                               //
+            if (e.KeyCode == Keys.Enter)                                                                                //
+            {                                                                                                           //
+                string get_string = textBox1.Text;                                  // получаем текст                   //
+                string processed_string = Processing.ProcessData(get_string);       // обрабатываем                     // контроллер
+                label1.Text = processed_string;                                     // выводим                          //
+            }                                                                                                           //
+        }                                                                                                               //
+    }                                                                                                                   //
+}                                                                                                                       //
---
+++ MVC/Processing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC
+{
+    class Processing                                            // модель (логика и обработка)
+    {
+        public static string ProcessData(string get_string)
+        {
+            return get_string.ToUpper();
+        }
+    }
+}
